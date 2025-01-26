196dcd8ead1bb5ec415f8716e4ef5f9701e0882a
Laila Seif <lailaseif710@gmail.com>
2025-01-25 22:22:32 -0500

    javascript animations
    added simple rotations to create fun jumping objects. went in and figured out what group (<g>) needed what ID.
    ...
    this had to be...one of the most difficult tasks out of this entire project... I managed to figure it out once using what i remembered and looked up and STUCK WITH IT.

--- js/main.js
+++ js/main.js
@@ -35,3 +35,154 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   });
   
+
+
+  /* animations */
+
+
+  /*--almond regular--*/
+
+  document.getElementById("rotate-almond-1").addEventListener("mouseenter", function() {
+    this.style.transform = "rotate(10deg)";
+});
+
+document.getElementById("rotate-almond-1").addEventListener("mouseleave", function() {
+    this.style.transform = "rotate(0deg)";
+});
+
+document.getElementById("rotate-almond-2").addEventListener("mouseenter", function() {
+    this.style.transform = "rotate(-5deg)";
+});
+
+document.getElementById("rotate-almond-2").addEventListener("mouseleave", function() {
+    this.style.transform = "rotate(0deg)";
+});
+
+
+/*--pistachio--*/
+
+document.getElementById("rotate-pista-1").addEventListener("mouseenter", function() {
+    this.style.transform = "rotate(10deg)";
+});
+
+document.getElementById("rotate-pista-1").addEventListener("mouseleave", function() {
+    this.style.transform = "rotate(0deg)";
+});
+
+document.getElementById("rotate-pista-2").addEventListener("mouseenter", function() {
+    this.style.transform = "rotate(-5deg)";
+});
+
+document.getElementById("rotate-pista-2").addEventListener("mouseleave", function() {
+    this.style.transform = "rotate(0deg)";
+});
+
+
+
+/*--macadamia--*/
+
+
+document.getElementById("rotate-maca-1").addEventListener("mouseenter", function() {
+    this.style.transform = "rotate(-10deg)";
+});
+
+document.getElementById("rotate-maca-1").addEventListener("mouseleave", function() {
+    this.style.transform = "rotate(0deg)";
+});
+
+document.getElementById("rotate-maca-2").addEventListener("mouseenter", function() {
+    this.style.transform = "rotate(5deg)";
+});
+
+document.getElementById("rotate-maca-2").addEventListener("mouseleave", function() {
+    this.style.transform = "rotate(0deg)";
+});
+
+/*--hazelnut--*/
+
+
+document.getElementById("rotate-hazel").addEventListener("mouseenter", function() {
+    this.style.transform = "rotate(5deg)";
+});
+
+document.getElementById("rotate-hazel").addEventListener("mouseleave", function() {
+    this.style.transform = "rotate(0deg)";
+});
+
+document.getElementById("rotate-hazelnut").addEventListener("mouseenter", function() {
+    this.style.transform = "rotate(-10deg)";
+});
+
+document.getElementById("rotate-hazelnut").addEventListener("mouseleave", function() {
+    this.style.transform = "rotate(0deg)";
+});
+
+
+/*--almond vegan--*/
+
+document.getElementById("rotate-almond-v-1").addEventListener("mouseenter", function() {
+    this.style.transform = "rotate(10deg)";
+});
+
+document.getElementById("rotate-almond-v-1").addEventListener("mouseleave", function() {
+    this.style.transform = "rotate(0deg)";
+});
+
+document.getElementById("rotate-almond-v-2").addEventListener("mouseenter", function() {
+    this.style.transform = "rotate(-5deg)";
+});
+
+document.getElementById("rotate-almond-v-2").addEventListener("mouseleave", function() {
+    this.style.transform = "rotate(0deg)";
+});
+
+document.getElementById("rotate-almond-v-leaf1").addEventListener("mouseenter", function() {
+    this.style.transform = "rotate(-5deg)";
+});
+
+document.getElementById("rotate-almond-v-leaf1").addEventListener("mouseleave", function() {
+    this.style.transform = "rotate(0deg)";
+});
+
+document.getElementById("rotate-almond-v-leaf2").addEventListener("mouseenter", function() {
+    this.style.transform = "rotate(5deg)";
+});
+
+document.getElementById("rotate-almond-v-leaf2").addEventListener("mouseleave", function() {
+    this.style.transform = "rotate(0deg)";
+});
+
+
+/*--coconut vegan--*/
+
+document.getElementById("rotate-coco-1").addEventListener("mouseenter", function() {
+    this.style.transform = "rotate(-10deg)";
+});
+
+document.getElementById("rotate-coco-1").addEventListener("mouseleave", function() {
+    this.style.transform = "rotate(0deg)";
+});
+
+document.getElementById("rotate-coco-2").addEventListener("mouseenter", function() {
+    this.style.transform = "rotate(5deg)";
+});
+
+document.getElementById("rotate-coco-2").addEventListener("mouseleave", function() {
+    this.style.transform = "rotate(0deg)";
+});
+
+document.getElementById("rotate-coco-leaf1").addEventListener("mouseenter", function() {
+    this.style.transform = "rotate(-5deg)";
+});
+
+document.getElementById("rotate-coco-leaf1").addEventListener("mouseleave", function() {
+    this.style.transform = "rotate(0deg)";
+});
+
+document.getElementById("rotate-coco-leaf2").addEventListener("mouseenter", function() {
+    this.style.transform = "rotate(-5deg)";
+});
+
+document.getElementById("rotate-coco-leaf2").addEventListener("mouseleave", function() {
+    this.style.transform = "rotate(0deg)";
+});
